ce22f19cfc6103881fa09455e915a749c300736c
Iroh Manzano <manzanoiroh777@gmail.com>
2025-01-07 21:04:03 +0800

    installed vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "Iroh Manzano - Portfolio"
@@ -14,6 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <Analytics />
       </body>
     </html>
   );
